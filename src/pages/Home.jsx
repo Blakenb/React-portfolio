d3c3fd4fa617d4e864ea04a1325294b1d7f85c09
Blakenb <blakenb87@gmail.com>
2025-03-12 21:24:42 -0500

    added to the home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,11 @@ const Home = () => (
     <h1>Welcome to My Portfolio,<br/>
     My name is Blake Brittain!
     </h1>
-    <p>I am a passionate developer who loves to code. I am a proud father of two children, and a lover of all the bully breeds (pit bulls are my favorite). In my free time, I love to get out and take pictures of all things in nature. 
+    <p>I am a passionate developer who loves to code. I am a proud husband to a wonderful wife who has pushed me to follow my dreams of getting into tech. She has been my biggest supporter and motivator throughout this journey. I am also a father of two beautiful children, and a lover of all the bully breeds dogs(pit bulls are my favorite). In my free time, I love to get out and take pictures of all things in nature, if I am not out taking pictures or spending time with my family I am sitting at my computer coding away. I am currently in a coding bootcamp at the University of Texas , where I am learning full-stack web development.
       <br/>
       <br />
-   I am new to the world of coding and tech, but I am excited to learn and grow in this field. I am currently learning HTML, CSS, and JavaScript. I am also learning React, Node.js, and Express.js. I am excited to learn more and to grow in this field. I am excited to see where this journey takes me. if you would like to know more about me, please feel free to reach out to me. I would love to hear from you.
-   </p>
+      I am new to the world of coding and tech, but I am excited to learn and grow in this field. I am currently learning HTML, CSS, and JavaScript. I am also learning React, Node.js, and Express.js. I am excited to learn more and to grow in this field. I am excited to see where this journey takes me. If you would like to know more about me, please feel free to reach out to me. I would love to hear from you.
+    </p>
   </section>
 );
 
