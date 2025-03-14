1af9d64328a5c4210860f47b9bbb0ded528d1644
Blakenb <blakenb87@gmail.com>
2025-03-13 19:05:10 -0500

    added my linkedIn

--- src/components/GitHubProfile.jsx
+++ src/components/GitHubProfile.jsx
@@ -6,6 +6,10 @@ const GitHubProfile = () => (
     <a href="https://github.com/Blakenb" target="_blank" rel="noopener noreferrer">
       Visit my GitHub Profile
     </a>
+    <br />
+    <a href="https://www.linkedin.com/in/blake-brittain-15329934b/" target="_blank" rel="noopener noreferrer">
+      Visit my LinkedIn Profile
+    </a>
   </div>
 );
 
